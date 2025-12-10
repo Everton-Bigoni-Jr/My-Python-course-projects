8cab0f4841c059c1563d06ac8792b4cb555f7516
Everton Bigoni Junior <evertonbgjunior@gmail.com>
2025-12-10 15:30:10 -0400

    Refactor tipos.py with improved output and comments

--- Aula 02/tipos.py
+++ Aula 02/tipos.py
@@ -1,22 +1,37 @@
-#---------------------------------------
-print("Hello World :)") #CODE BY @evertin_bg
-#---------------------------------------
+print("------------------------------------------")
+print("    Hello World | 👑-JESUS is KING-👑") # CODE BY @evertin_bg
+print("------------------------------------------")
 
-
+# Exemplo de tipagem de váriaveis em Python
+# -------------------------
 # Tipos de dados básicos
 idade = 25
 altura = 1.75
 nome = "João"
 is_estudante = True
-#---------------------------------------
 
-
-# Exemplo de tipagem de variávies em Python
-print(type(idade))
+# Verificando tipos de dados
+print(type(idade)) 
 print(type(altura))
 print(type(nome))
 print(type(is_estudante))
-#---------------------------------------
 
 
- 
+# ----------------------- (Tipos de classes em Python) ----------------------- #
+
+# 1. int: Números inteiros (ex.. 5, -10, 6, 60, -100)
+# ---------------------------------------------------------------------------- #
+# 2. float: Números de ponto flutuante(quebrado) (ex.. 3.14, 5.01, -40.6) 
+# ---------------------------------------------------------------------------- # 
+# 3. str: Cadeias de caracteres ou strings (ex.. "Python" , "Olá", "Everton")
+# ---------------------------------------------------------------------------- #
+# 4. bool: Valores booleanos (ex.. True, False) Verdadeiro ou Falso
+# ---------------------------------------------------------------------------- # 
+# 5. list: Listas, sequências mútaveis (ex.. [1, 2, 3])
+# ---------------------------------------------------------------------------- # 
+# 6. tuple: Tuplas, sequências imútaveis (ex.. (1, 2, 3))
+# ---------------------------------------------------------------------------- # 
+# 7. dict: Dicionários, para chaves-valor (ex.. {"nome", "Ana", "Idade", 30 })
+# ---------------------------------------------------------------------------- # 
+# 8. set: Conjuntos, coleções não ordenandas sem duplicatas (ex.. {1, 2, 3})
+# ---------------------------------------------------------------------------- #
